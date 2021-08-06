35aae0a1447a0feb3c11748501e40e9c5c395bba
codeolam <frankuchenna30@gmail.com>
2021-08-06 10:19:42 +0100

    created add category function

--- src/components/addCategory.tsx
+++
@@ -1,9 +0,0 @@
-function AddCategory(){
-    return(
-        <div>
-            <h1>Add Category</h1>
-        </div>
-    )
-}
-
-export default AddCategory
--- src/components/allCategory.tsx
+++ src/components/allCategory.tsx
@@ -1,23 +1,68 @@
 import React, {useEffect, useState} from "react";
 import {DataStore} from 'aws-amplify';
 import { Category } from "../models";
-import sortBy from 'lodash/sortBy';
+import {Button, Input} from 'antd';
+// import sortBy from 'lodash/sortBy';
+
+const initialState ={
+    name: ''
+}
+
+interface targetInput {
+    name: string;
+    value: string
+}
 
 function Categories(){
     const [categories, setCategories] = useState<Category[]>([])
+    const [formState, setFormState] = useState(initialState)
+
+    //This function handles adding of new category
+    async function addCategory(){
+        if (!formState) return
+        await DataStore.save(new Category({...formState}))
+        setFormState(initialState)
+    }
+
+    function onChange(e: { target: targetInput }){
+        setFormState({... formState, [e.target.name]: e.target.value})
+    }
+    //Add new category end logic
+
+    //Query all category logic begins
+    async function fetchCategories(){
+        const categories = await DataStore.query(Category)
+        setCategories(categories)
+    }
+    //End query
 
     useEffect(() =>{
-        DataStore.query(Category).then(
-            (categories)=> setCategories(sortBy(categories, 'createdAt'))
-        )
-    })
+        // DataStore.query(Category).then(
+        //     (categories)=> setCategories(sortBy(categories, 'createdAt'))
+        // )
+        fetchCategories()
+        const subscription = DataStore.observe(Category).subscribe(
+            () => fetchCategories())
+            return () => subscription.unsubscribe()
+    }, [])
 
     return(
         <div>
+            <h2>Add New Category</h2>
+            <Input 
+                onChange={onChange}
+                name='name'
+                placeholder="Category Name"
+                value={formState.name}
+            />
+            <Button type="primary" onClick={addCategory}>Add Category</Button>
             <h1>All Category</h1>
             {
                 categories.map(cat => (
-                    <li>{cat.name}</li>)
+                    <div key={cat.id}> 
+                    <li>{cat.name}</li>
+                    </div>
+                    )
                 )
             }
         </div>
