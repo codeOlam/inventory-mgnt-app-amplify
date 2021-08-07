fbfb7e7992a06a289544a0d41e74680ceb736c78
codeolam <frankuchenna30@gmail.com>
2021-08-07 08:42:08 +0100

    updated componenet files for product pages

---
+++ src/pages/product-pages/availableProducts.tsx
@@ -0,0 +1,7 @@
+function AvailableProducts (){
+    return (
+        <h1>Product Table for InSotck Items</h1>
+    )
+}
+
+export default AvailableProducts
---
+++ src/pages/product-pages/soldProduct.tsx
@@ -0,0 +1,7 @@
+function SoldProducts (){
+    return (
+        <h1>Product Table for InSotck Items</h1>
+    )
+}
+
+export default SoldProducts
