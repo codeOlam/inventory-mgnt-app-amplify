652d58c236f9b8028ba74d5c6d3d6187b139ff69
codeolam <frankuchenna30@gmail.com>
2021-08-07 08:39:44 +0100

    fixed conflict issues on product models

--- src/models/index.d.ts
+++ src/models/index.d.ts
@@ -15,9 +15,10 @@ type CategoryMetaData = {
 export declare class Product {
   readonly id: string;
   readonly name: string;
-  readonly description?: string;
+  readonly description: string;
   readonly price: number;
-  readonly inStock?: boolean;
+  readonly inStock: boolean;
+  readonly categoryId: string;
   readonly createdAt?: string;
   readonly updatedAt?: string;
   readonly category?: Category;
