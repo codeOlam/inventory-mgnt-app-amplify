676e09c5d1f10fe6e6c47bfb3bd04d802601e0fd
codeolam <frankuchenna30@gmail.com>
2021-08-08 12:55:25 +0100

    fixed inline delete for table

--- src/category/delete.tsx
+++ src/category/delete.tsx
@@ -1,6 +1,33 @@
+import { useEffect, useState } from "react";
+import { DataStore } from "@aws-amplify/datastore"
+import { Category } from "../models"
+import { DeleteTwoTone } from "@ant-design/icons";
+
 function DeleteCate(){
+    const [categories, setCategories] = useState<Category[]>([])
+    
+    async function deleteCategory(id: string){
+        try{
+            const category = await DataStore.query(Category, id)
+            console.log(`category to be deleted is: ${category?.name}`)
+            DataStore.delete(category!)
+            console.log(`${category?.name} has been removed successfully`)
+        }catch(error){
+            console.log(`error deleting ${Category.name}`)
+        }
+        
+    }
+
     return(
-        <h1>Delete cate anchor ref icon</h1>
+        <div>
+            <a
+            {
+                ...categories.map(cat => (
+                    <a onClick={() => deleteCategory(cat.id)}>delete</a>
+                    )
+                )
+            }> delete </a>
+        </div>
     )
 }
 
--- src/category/list.tsx
+++ src/category/list.tsx
@@ -1,14 +1,7 @@
 import React, {useEffect, useState} from "react";
 import {DataStore} from 'aws-amplify';
 import { Category } from "../models";
-import { 
-    Layout, 
-    Breadcrumb, 
-    Table, 
-    Tag, 
-    Spin,
-    Popconfirm, 
-    Popover} from "antd";
+import { Table, Spin, Popconfirm, Popover} from "antd";
 import { DeleteTwoTone, EditTwoTone } from "@ant-design/icons";
 
 function ListCate(){
@@ -29,25 +22,36 @@ function ListCate(){
             return () => subscription.unsubscribe()
     }, [])
 
+    function selectItem(item: string){
+        console.log('Item is: ', item)
+    }
+
+    async function deleteCategory(id: string){
+        console.log('item id:', id)
+
+        try{
+            const category = await DataStore.query(Category, id)
+            console.log(`category to be deleted is: ${category?.name}`)
+            DataStore.delete(category!)
+            console.log(`${category?.name} has been removed successfully`)
+        }catch(error){
+            console.log(`error deleting ${Category.name}`)
+        }
+        
+    }
+
     const columns = [
         {title: 'Name', dataIndex: 'name', key: 'name'},
         {title: 'CreatedAt', dataIndex: 'createdAt', key: 'createdAt'},
         {title: 'UpdatedAt', dataIndex: 'updatedAt', key: 'updatedAt'},
-        {
-            title: 'Actions', 
-            dataIndex: '', 
-            key: 'x', 
-            render: () =><>
-            <Popconfirm title="Confirm item removal?" key='x'><a><DeleteTwoTone /></a></Popconfirm>
-            <Popover content="Update item status" key='x'> <a><EditTwoTone /></a></Popover>
-            </>
-        },
+        {title: 'Actions', dataIndex: 'operation', key: 'operation'},
     ];
 
     const data = categories?.map(row => ({
         name: row.name,
         createdAt: row.createdAt,
-        updatedAt: row.updatedAt
+        updatedAt: row.updatedAt,
+        operation: <><a onClick={() => deleteCategory(row.id)}><DeleteTwoTone/></a> <a onClick={() => deleteCategory(row.id)}><EditTwoTone /></a></>
     }))
 
     return (
